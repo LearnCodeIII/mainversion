66031b969aa4d1280849a7ff96c097284b226a38
poutruru <poutruru@users.noreply.github.com>
2019-03-20 15:14:37 +0800

    search還沒好

---
+++ php/article_insert2test.php
@@ -0,0 +1,133 @@
+<?php
+$pagename = "pageMain";
+
+include __DIR__.'/PDO.php';
+?>
+<?php include __DIR__.'./head.php'?>
+<?php include __DIR__.'./nav.php'?>
+<?php include __DIR__.'./RuthNav.php'?>
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+<link rel="stylesheet" href="../css/jquery-ui.css">
+
+
+
+
+<script>
+$(document).ready(function() {
+    bsCustomFileInput.init()
+});
+
+$(function() {
+    $("#datepicker").datepicker({
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        dateFormat: 'yy-mm-dd'
+    });
+});
+</script>
+
+<style>
+#editor-container {
+  height: 375px;
+}
+
+</style>
+
+<section class="dashboard">
+    <div class="container">
+        <div id="infoMsg" class="alert alert-<?=$msg['type']?>" role="alert" style="display:none">
+        </div>
+        <?php if(isset($msg)): ?>
+        <div class="alert alert-<?=$msg['type']?>" role="alert">
+            <?= $msg['info'] ?>
+        </div>
+        <?php endif?>
+        <form name="form1" method="post" onsubmit="return postya();">
+            <label>Title</label>
+            <div>
+                <input type="text" class="form-control mb-2" id="title" name="title" placeholder="title">
+                <small id="titleHelp" class="form-text text-muted"></small>
+            </div>
+            <label>Author</label>
+            <div class="form-row">
+                <div class="col-6">
+                    <input type="text" class="form-control" id="author" name="author" placeholder="作者" value="">
+                    <small id="authorHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="col-6">
+                    <input type="text" class="form-control" id="avatar" name="avatar" placeholder="作者頭像" value="">
+                    <small id="avatarHelp" class="form-text text-muted"></small>
+                </div>
+            </div>
+
+            <label>Date</label>
+            <div class="form-row">
+                <div class="col-6">
+                    <input type="text" class="form-control mb-2" id="datepicker" name="date"
+                        placeholder="請輸入日期 YY-MM-DD">
+                    <small id="dateHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="custom-file col-6">
+                    <input type="file" class="custom-file-input" id="image" name="image">
+                    <label class="custom-file-label" for="image" data-browse="upload"></label>
+                    <small id="imageHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="col-12">
+                <input name="content" type="hidden">
+                <div id="editor-container">
+                </div>
+                </div>
+
+                <label>Link</label>
+
+                    <input type="text" class="form-control mb-2" id="link" name="link" placeholder="link">
+                    <small id="linkHelp" class="form-text text-muted"></small>
+                
+                <button id="submitBtn" type="submit" class="btn btn-primary my-2">Submit</button>
+        </form>
+</section>
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<script>
+var quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ]
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'  // or 'bubble'
+});
+
+const postya = () => {
+    submitBtn.style.display = 'none';
+
+    var form1 = new FormData(document.form1);
+
+    var content = document.querySelector('input[name=content]');
+    content.value = quill.getContents();
+
+    fetch('article_insert_api.php', {
+            method: 'POST',
+            body: form1
+        })
+        .then(response => response.json())
+        .then(obj => {
+            console.log(obj);
+            infoMsg.style.display = 'block';
+            if (obj.success) {
+                infoMsg.className = 'alert alert-success';
+                infoMsg.innerHTML = '資料新增成功';
+            } else {
+                infoMsg.className = 'alert alert-dager';
+                infoMsg.innerHTML = obj.errorMsg;
+            }
+            submitBtn.style.display = 'block';
+        });
+    return false;
+};
+</script>
+
+
+<!-- <?php include __DIR__.'/__html_foot.php'; ?> -->
---
+++ php/article_search_api.php
@@ -0,0 +1,54 @@
+<?php 
+require __DIR__.'/PDO.php';
+
+$result = [
+    'success' => false,
+    'page' => 0,
+    'perPage' => 0,
+    'totalPages' => 0,
+    'totalRows' => 0,
+    'data' => [],
+    ];
+
+$pname = 'article_list';
+$per_page = 10;
+ //每頁筆數
+$result['perPage'] = $per_page;
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1 ; //用戶輸入的頁數
+
+$searchkey = isset($_POST['searchkey']) ? intval($_POST['searchkey']) : 0;
+$srky = "'%".$searchkey."%'";
+
+if (!isset($searchkey)){
+    $result["errorMsg"] = '搜尋到0筆資料';
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+//總筆數
+$t_sql = sprintf("SELECT COUNT(1) FROM `article`  WHERE `title` LIKE %s OR `content` LIKE %s",$srky,$srky);
+$t_stmt = $pdo -> query($t_sql);
+$t_rows = $t_stmt -> fetch(PDO::FETCH_NUM)[0];
+$result['totalRows'] = intval($t_rows);
+
+//總頁數
+$total_pages = ceil($t_rows/$per_page);
+$result['totalPages'] = $total_pages;
+
+if($page < 1)  $page = 1 ;
+if($page > $total_pages) $page = $total_pages;
+$result['page'] = $page;
+
+$sql = sprintf("SELECT * FROM `article` WHERE `title` LIKE %s OR `content` LIKE %s ORDER BY sid DESC LIMIT %s, %s", $srky,$srky,($page-1)*$per_page, $per_page);
+$stmt = $pdo -> query($sql);
+
+$rows = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+$result['data'] = $rows;
+$result['success'] = true;
+
+// echo json_encode($result) ;
+// 將陣列轉換成 json 字串      ↓跳脫 字碼
+echo json_encode($result, JSON_UNESCAPED_UNICODE);   
+
+?>
