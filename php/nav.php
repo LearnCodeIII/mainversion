2202565695f7539a63ef2e3cf9961194bfee0728
ShawnLin <shawnlin0201@gmail.com>
2019-03-20 11:04:05 +0800

    navbar 連結設定完成
    更新LOGO href
    置換影片系統的 href

--- php/nav.php
+++ php/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">.Moiveee</a>
+    <a class="navbar-brand" href="mainpage.php">.Moiveee</a>
 	<div class="container-fulid">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +14,7 @@
 					<a class="nav-link" href="cinema_ifmt_list.php">戲院系統</a>
 				</li>
 				<li class="nav-item <?= $pagename == "pageData"?"active":""; ?>">
-					<a class="nav-link" href="film_data_list.php">影片系統</a>
+					<a class="nav-link" href="film_main.php">影片系統</a>
 				</li>
 				<li class="nav-item <?= $pagename == "pageData"?"active":""; ?>">
 					<a class="nav-link" href="RuthPageMain.php">文章系統</a>
