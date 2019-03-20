1d18a736308031872e029187a83af5638fb6a5f3
ShawnLin <shawnlin0201@gmail.com>
2019-03-20 10:57:46 +0800

    主路口

---
+++ php/mainpage.php
@@ -0,0 +1,15 @@
+<?php
+$pagename = "member";
+
+include __DIR__.'/PDO.php';
+
+
+
+?>
+<?php include __DIR__.'./head.php'?>
+<?php include __DIR__.'./nav.php'?>
+<?php include __DIR__.'./sidenav.php'?>
+<section class="dashboard">
+
+</section>
+<?php include __DIR__.'./foot.php'?>
