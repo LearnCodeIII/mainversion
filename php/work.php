eb60e1d62a9bc6a784743c3cf7d1a5e3fdadfb0c
ShawnLin <shawnlin0201@gmail.com>
2019-03-22 12:44:41 +0800

    Update work.php

--- php/work.php
+++ php/work.php
@@ -13,7 +13,6 @@ include __DIR__.'/PDO.php';
 <table class="table">
   <thead class="thead-dark">
     <tr>
-      <th scope="col">#</th>
       <th scope="col">姓名</th>
       <th scope="col">學號</th>
       <th scope="col">分工</th>
@@ -21,86 +20,65 @@ include __DIR__.'/PDO.php';
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
       <td>林奇璇</td>
       <td>07</td>
       <td>
         <ul>
-            <li>介面整合、github整合除錯、資料庫規劃</li>
-            <li>活動列表頁面、活動新增頁面、活動預覽頁面、登入功能</li>
-            <li></li>
+            <li>github整合除錯、資料庫規劃、活動資料整理、活動後臺、後臺框架設計、分工表</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">2</th>
       <td>陸昱如</td>
       <td>05</td>
       <td>
         <ul>
-            <li>資料庫文章欄位設計、文章資料整理</li>
-            <li>後臺文章頁面、文章預覽頁面</li>
-            <li></li>
+            <li>資料庫文章欄位設計、文章資料整理、文章後臺</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">3</th>
       <td>蘇珈萱</td>
       <td>13</td>
       <td>
         <ul>
-        <li>資料庫戲院欄位設計、戲院資料整理</li>
-            <li>後臺文章頁面、文章預覽頁面</li>
-            <li></li>
+            <li>資料庫會員欄位設計、會員資料整理、會員後臺</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">3</th>
       <td>廖上源</td>
       <td>23</td>
       <td>
         <ul>
-            <li>資料庫戲院欄位設計、戲院資料整理</li>
-            <li>後臺文章頁面、文章預覽頁面</li>
-            <li></li>
+            <li>資料庫戲院欄位設計、戲院資料整理、戲院後臺</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">3</th>
       <td>杜冠葦</td>
       <td>27</td>
       <td>
         <ul>
-            <li></li>
-            <li>後臺文章頁面、文章預覽頁面</li>
-            <li></li>
+            <li>資料庫論壇欄位設計、論壇資料整理、論壇後臺</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">3</th>
       <td>王浩晴</td>
       <td>36</td>
       <td>
         <ul>
-            <li></li>
-            <li>後臺文章頁面、文章預覽頁面</li>
-            <li></li>
+            <li>資料庫影片欄位設計、影片資料整理、影片後臺</li>
         </ul>
       </td>
     </tr>
     <tr>
-      <th scope="row">3</th>
       <td>徐妤安</td>
       <td>41</td>
       <td>
         <ul>
-            <li></li>
-            <li>後臺廣告頁面、廣告預覽頁面</li>
-            <li></li>
+            <li>資料庫廣告欄位設計、廣告資料整理、廣告後臺</li>
         </ul>
       </td>
     </tr>
