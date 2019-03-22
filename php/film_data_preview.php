ae43a85de9ad7f5b394ad2996c89c349852c2f42
charleswanghc <48010741+charleswanghc@users.noreply.github.com>
2019-03-22 09:40:11 +0800

    Update film_data_preview.php

--- php/film_data_preview.php
+++ php/film_data_preview.php
@@ -18,7 +18,7 @@ $row = $stamt->fetch(PDO::FETCH_ASSOC);
 
 include __DIR__.'./head.php';
 include __DIR__.'./nav.php';
-include __DIR__.'./sidenav.php';
+include __DIR__.'./film_sidenav.php';
 ?>
 <style>
     .form-group small {
