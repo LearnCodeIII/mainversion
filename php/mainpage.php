8031b8748ed96097c581d912fd369f2eb01281bd
yuru lu <poutruru@gmail.com>
2019-04-05 21:04:59 +0800

    123

--- php/mainpage.php
+++ php/mainpage.php
@@ -7,9 +7,12 @@ include __DIR__.'/PDO.php';
 
 ?>
 <?php include __DIR__.'./head.php'?>
-<?php include __DIR__.'./nav.php'?>
 <?php include __DIR__.'./sidenav.php'?>
 <section class="dashboard">
+
+<div class="container mt-3">
+    <h1>我是mainpage</h1>
+</div>
 
 </section>
 <?php include __DIR__.'./foot.php'?>
