7b6ae8883dc549a3f21bc423da775ea03db6f088
charleswanghc <48010741+charleswanghc@users.noreply.github.com>
2019-04-05 19:00:30 +0800

    改Ru版
    加gropname去掉自己sidenav刪除nav

--- php/film_data_insert.php
+++ php/film_data_insert.php
@@ -3,12 +3,13 @@
 
 
 require __DIR__.'/PDO.php';
+
+$groupname = "film";
 $pagename = "film_data_insert";
 
 
 include __DIR__.'./head.php';
-include __DIR__.'./nav.php';
-include __DIR__.'./film_sidenav.php';
+include __DIR__.'./sidenav.php'
 ?>
 <style>
     .form-group small {
--- php/film_data_list.php
+++ php/film_data_list.php
@@ -1,12 +1,14 @@
 <?php
 // require __DIR__. '/film_crud_session.php';
 #頁面名稱
+$groupname = "film";
 $pagename = "film_data_list";
 
 include __DIR__.'/PDO.php';
+
 include __DIR__.'./head.php';
-include __DIR__.'./nav.php';
-include __DIR__.'./film_sidenav.php';
+include __DIR__.'./sidenav.php'
+
 
 if(isset($_SESSION['admin'])){
     $user = "小編：";
