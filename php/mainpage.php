9c07e77811c60d51e4d508b44ee28ea12b9d7a8a
ShawnLin <shawnlin0201@gmail.com>
2019-04-08 12:29:41 +0800

    工作表

--- php/mainpage.php
+++ php/mainpage.php
@@ -80,6 +80,7 @@ html,body{
         <p style="color:white" class="dashboardLogoContents dashboardLogoContent2">.Everything</p>
         <p style="color:white" class="dashboardLogoContents dashboardLogoContent3">.Everyday</p>
         <p style="color:white" class="dashboardLogoContents dashboardLogoContent4">.Everynight</p>
+        <p style="color:white" class="dashboardLogoContents dashboardLogoContent5"><a href="http://192.168.27.179/mainversion/mainversion/php/work.php">.找工作表?</a></p>
 
     </div>
 </section>
@@ -90,10 +91,8 @@ html,body{
     $('.dashboardLogoContents').animate({
             "textIndent":"800px"
         });
+    let secret=0;
     $('.dashboardLogo').on("mouseover",function(){
-        // $('.dashboardLogoContent').animate({
-            //     "fontSize":"5rem",
-            // });
         $('.dashboardLogoContent').animate({
             "fontSize":"4rem"
         });
@@ -109,6 +108,14 @@ html,body{
         $('.dashboardLogoContent4').animate({
             "textIndent":""
         },2200);
+        if(secret>10){
+            $('.dashboardLogoContent5').animate({
+                "textIndent":""
+            },1000);
+        }
+
+        secret+=1;
+        console.log(secret)
     })
 
 
